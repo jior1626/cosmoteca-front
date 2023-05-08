f6a37c8bae8536f9276b93a8978a1bed5f93dc27
Jordan Orozco <joaorozc@bancolombia.com.co>
2023-05-08 17:57:53 -0500

    search books

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,27 +4,23 @@ import { useNavigate } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 
 import "./Search.css";
+import { useAppDispatch } from "../../redux/hooks";
+import { filterBooks } from "../../redux/states/book.slice";
+import { setLoading } from "../../redux/states/loading.slice";
 
 
 const Search: React.FC<any> = ({ }) => {
 
     const [filter, setFilter] = useState("");
 
-    const searchText = useRef('');
-    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
-    const handleSubmit = (e: any) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
-        // let tempSearchTerm = searchText.current.value.trim();
-        // if((tempSearchTerm.replace(/[^\w\s]/gi,"")).length === 0){
-        //   setSearchTerm("the lost world");
-        //   setResultTitle("Please Enter Something ...");
-        // } else {
-        //   setSearchTerm(searchText.current.value);
-        // }
-    
-        navigate("/book");
-      };
+        await dispatch(setLoading(true));
+        await dispatch(filterBooks(filter));
+        await dispatch(setLoading(false));
+    }
 
     return (
         <div className='search-form'>
@@ -32,7 +28,7 @@ const Search: React.FC<any> = ({ }) => {
                 <div className='search-form-content'>
                     <form className='search-form' onSubmit={handleSubmit}>
                         <div className='search-form-elem flex flex-sb bg-white'>
-                            <input type="text" className='form-control' placeholder='The Lost World ...' />
+                            <input type="text" className='form-control' placeholder='The Lost World ...' onChange={(e) => setFilter(e.target.value)} />
                             <button type="submit" className='flex flex-c' onClick={handleSubmit}>
                                 {/* <FaSearch className='text-purple' size={32} /> */}
                             </button>
